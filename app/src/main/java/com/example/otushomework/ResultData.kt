22400ca9dab572c38ed5ec34fdcd458e6aed375c
letter <letter.kirill@gmail.com>
2021-02-22 14:59:51 +0300

    add code

---
+++ app/src/main/java/com/example/otushomework/ResultData.kt
@@ -0,0 +1,5 @@
+package com.example.otushomework
+
+import java.io.Serializable
+
+data class ResultData(val checked: Boolean, val text: String): Serializable
---
+++ app/src/main/java/com/example/otushomework/SomeData.kt
@@ -0,0 +1,5 @@
+package com.example.otushomework
+
+import java.io.Serializable
+
+data class SomeData(val title:String, val idImage:Int):Serializable
